e3a7d3d3c2091caba0fc6325d623e50ecb642241
Nathan Cullen <Nathan.Cullen@autotrader.co.uk>
2022-12-19 12:24:46 +0000

    add strikes and spares

--- exercises/kotlin/src/main/kotlin/Bowling.kt
+++ exercises/kotlin/src/main/kotlin/Bowling.kt
@@ -1,8 +1,19 @@
 class Bowling {
     companion object {
 
-        fun bowl(bowls: IntArray): Int {
-            return bowls.sum()
+        fun bowl(bowls: List<Int>): Int {
+            var score = 0
+            val frames = bowls.windowed(2, 2)
+
+            fun isStrike(firstBowl: Int) = (firstBowl == 10)
+            fun isSpare(sumBowls: Int) = (sumBowls == 10)
+            for (frame in frames) {
+                val nextFrame = frames[frames.indexOf(frame) + 1]
+                if (isStrike(frame[0])) score += 10 + nextFrame.sum()
+                else if (isSpare(frame.sum())) score += 10 + nextFrame[0]
+                else score += frame.sum()
+            }
+            return score
         }
     }
 }
--- exercises/kotlin/src/test/kotlin/bowling/BowlingTest.kt
+++ exercises/kotlin/src/test/kotlin/bowling/BowlingTest.kt
@@ -7,14 +7,24 @@ import org.junit.jupiter.api.Test
 
 internal class BowlingTest {
     @Test
-    fun renameMe(){
-        val rolls = intArrayOf(0,0, 0,0, 0,0, 0,0, 0,0, 0,0, 0,0, 0,0, 0,0, 0,0);
+    fun scoreWithNoHits(){
+        val rolls = listOf(0,0, 0,0, 0,0, 0,0, 0,0, 0,0, 0,0, 0,0, 0,0, 0,0);
         assertThat(bowl(rolls)).isEqualTo(0)
     }
     @Test
     fun scoreWithNoStrikes(){
-        val rolls = intArrayOf(1, 2, 0,0, 0,0, 0,0, 0,0, 0,0, 0,0, 0,0, 0,0, 0,7)
+        val rolls = listOf<Int>(1, 2, 7,0, 0,0, 0,0, 0,0, 0,0, 0,0, 0,0, 0,0, 0,0)
         assertThat(bowl(rolls)).isEqualTo(10)
     }
-    
+    @Test
+    fun scoreWithSpare(){
+        val rolls = listOf(5,5, 5,2, 0,0, 0,0, 0,0, 0,0, 0,0, 0,0, 0,0, 0,0);
+        assertThat(bowl(rolls)).isEqualTo(22)
+    }
+    @Test
+    fun scoreWithStrike(){
+        val rolls = listOf(10,0, 4,4, 0,0, 0,0, 0,0, 0,0, 0,0, 0,0, 0,0, 0,0);
+        assertThat(bowl(rolls)).isEqualTo(26)
+    }
+
 }
