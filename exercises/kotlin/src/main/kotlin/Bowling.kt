0a35de006710678b06f3f3568e0f2dea24f179d6
Nathan Cullen <Nathan.Cullen@autotrader.co.uk>
2022-12-19 16:49:16 +0000

    fix bowling

--- exercises/kotlin/src/main/kotlin/Bowling.kt
+++ exercises/kotlin/src/main/kotlin/Bowling.kt
@@ -4,19 +4,16 @@ class Bowling {
         fun bowl(bowls: List<Int>): Int {
             var score = 0
             var frameIndex = 0
+            val frames = 0..9
             fun isStrike(firstBowl: Int) = (firstBowl == 10)
             fun isSpare(firstBowl: Int, secondBowl: Int) = (firstBowl + secondBowl == 10)
 
-            for (frame in 0..9) {
+            for (frame in frames) {
                 if (isStrike(bowls[frameIndex])) {
-                    if (frame == 9) {
-                        score += 10 + bowls[frameIndex + 1] + bowls[frameIndex + 2]
-                    }
+                    score += 10 + bowls[frameIndex + 1] + bowls[frameIndex + 2]
                     frameIndex++
                 } else if (isSpare(bowls[frameIndex], bowls[frameIndex + 1])) {
-                    if (frame == 9) {
-                        score += 10 + bowls[frameIndex + 2]
-                    }
+                    score += 10 + bowls[frameIndex + 2]
                     frameIndex += 2
                 } else {
                     score += bowls[frameIndex] + bowls[frameIndex + 1]
